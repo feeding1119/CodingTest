6c77ca166c25dc1aa78e597cfaa6f58d13403424
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2023-04-07 02:13:03 +0900

    [Silver III] Title: 주유소, Time: 88 ms, Memory: 42684 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,22 @@
+n = int(input())
+d = list(map(int,input().split()))
+m = list(map(int,input().split()))
+
+mm = 10001
+rindex = len(d)
+lindex = len(m)-1
+total = 0
+
+while lindex != 0:
+    for i in range(lindex):
+        if mm > m[i] :
+            mm = m[i]
+            lindex = i
+
+    total += mm*(sum(d[lindex:rindex]))
+    rindex = lindex
+    mm = 10001
+
+print(total)
+
+
