b8c5de76fae8ea2309dc6c3706607aa8c26bcdf8
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2023-04-07 02:58:55 +0900

    Update 주유소.py

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,18 +2,13 @@ n = int(input())
 d = list(map(int,input().split()))
 m = list(map(int,input().split()))
 
-mm = 10001
+mm = m[0]
 total = 0
 
-
-for i in range(len(m)-1):
+for i in range(n-1):
     if m[i] < mm :
         mm = m[i]
-        if m[i] == 1:
-            total += mm*sum(d[i:])
-            break
+
     total += mm*d[i]
 
 print(total)
-
-
