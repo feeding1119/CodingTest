9fc131e51c694831479ac80df0728d17ba16c63d
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2023-05-21 04:54:39 +0900

    [Silver III] Title: N과 M (1), Time: 192 ms, Memory: 36688 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,22 @@
+import sys
+from collections import deque
+from heapq import heappop,heappush
+import math
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+
+s = []
+
+def dfs():
+    if len(s) == M:
+        print(' '.join(map(str,s)))
+        return
+
+    for i in range(1,N+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+dfs()
