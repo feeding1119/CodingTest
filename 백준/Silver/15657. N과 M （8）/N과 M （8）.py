40316011e863b55ebf0906e09a8c8680312e46f3
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2023-05-29 21:37:54 +0900

    [Silver III] Title: N과 M (8), Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+N,M = map(int,input().split())
+numlist = list(map(int,input().split()))
+answer = []
+numlist.sort()
+
+def dfs(start):
+    if len(answer) == M:
+        print(' '.join(map(str,answer)))
+        return
+    for i in range(start,N):
+        answer.append(numlist[i])
+        dfs(i)
+        answer.pop()
+
+dfs(0)
