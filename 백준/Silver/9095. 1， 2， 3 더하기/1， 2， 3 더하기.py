b9a19cdcf0d4a670f56381e7b8c8a457cf4e906f
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-01-17 19:30:26 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -1,30 +1,18 @@
-import sys
-from collections import deque
-from heapq import heappop,heappush
-import math
-
-input = sys.stdin.readline
-
 T = int(input())
+
+num_list = []
 
 for _ in range(T):
     n = int(input())
 
-    dp = [0]*(n+1)
-
-    if n == 1 :
+    if(n==1):
         print(1)
-    elif n == 2 :
-        print(2)
-    elif n == 3:
-        print(4)
+        continue
+    dp = [1] * (n+1)
 
-    else:
-        dp[1] = 1
-        dp[2] = 2
-        dp[3] = 4
+    dp[2] = 2
 
-        for i in range(4,n+1):
-            dp[i] = dp[i-1]+dp[i-2]+dp[i-3]
+    for i in range(3,n+1):
+        dp[i] = dp[i-3] + dp[i-2] + dp[i-1]
 
-        print(dp[n])
+    print(dp[n])
