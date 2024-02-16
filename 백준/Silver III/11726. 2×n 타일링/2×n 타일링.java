61333e9b7a10bbdf0f8adc42fee39f4350ee7a2a
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-02-16 11:22:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 132 ms, Memory: 14300 KB -BaekjoonHub

---
+++ 백준/Silver III/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static final int mod = 10007;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2; i<=n;i++){
+            dp[i] = (dp[i-1] + dp[i-2])%mod;
+        }
+
+        System.out.println(dp[n]);
+    }
+}
