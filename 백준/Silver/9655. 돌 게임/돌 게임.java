e80cef2cdd40b3d1c1450eef8b27ff0dc3964ed1
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-02-19 19:09:10 +0900

    [Silver V] Title: 돌 게임, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

--- 백준/Silver/9655. 돌 게임/돌 게임.java
+++ 백준/Silver/9655. 돌 게임/돌 게임.java
@@ -6,17 +6,17 @@ public class Main {
 
         int N = sc.nextInt();
 
-        int[] dp = new int[1001];
+        int[] dp = new int[N+4];
 
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 1;
+        dp[1]=1;
 
-        for(int i=4;i<=N;i++){
-            if(dp[i-1]==1||dp[i-3]==1){
-                dp[i] = 2;
+        for(int i=2;i<=N;i++){
+            if(dp[i]==1){
+                dp[i+1] = 2;
+                dp[i+3] = 2;
             }else{
-                dp[i] = 1;
+                dp[i+1] = 1;
+                dp[i+3] = 1;
             }
         }
 
