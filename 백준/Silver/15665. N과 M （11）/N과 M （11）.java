c851712ff628fcf718fa1a4fae284a7b23e72620
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-02-21 18:45:08 +0900

    [Silver II] Title: N과 M (11), Time: 396 ms, Memory: 34196 KB -BaekjoonHub

---
+++ 백준/Silver/15665. N과 M （11）/N과 M （11）.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] nArr;
+    static int[] resultArr;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        nArr = new int[N];
+        resultArr = new int[M];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i=0;i<N;i++){
+            nArr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(nArr);
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    static void dfs(int index){
+        if(index == M){
+            for(int result: resultArr){
+                sb.append(result).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        int prevValue = 0;
+
+        for(int i=0;i<N;i++){
+            if(prevValue!=nArr[i]){
+                prevValue = nArr[i];
+                resultArr[index] = nArr[i];
+                dfs(index+1);
+            }
+        }
+
+    }
+}
+
