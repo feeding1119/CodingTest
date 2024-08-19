0031a616b63ce0b7c0561e034315d484c5a7c0a7
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-08-19 15:00:04 +0900

    [Silver V] Title: 올림픽, Time: 96 ms, Memory: 14160 KB, Score: 12 point -BaekjoonHub

--- 백준/Silver/8979. 올림픽/올림픽.java
+++ 백준/Silver/8979. 올림픽/올림픽.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -54,7 +55,7 @@ public class Main {
         }
 
         int count = 0;
-        for(int i=answer; i>=0;i--){
+        for(int i=answer-1; i>=0;i--){
             if(medal[i][0] == medal[answer][0] && medal[i][1] == medal[answer][1] && medal[i][2] == medal[answer][2]){
                 count++;
             }else break;
