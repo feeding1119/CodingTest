60568ab54390ff02a2d1e9653c20913e95765c7d
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2024-09-28 14:14:25 +0900

    [level 2] Title: 소수 찾기, Time: 19.55 ms, Memory: 80.3 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,45 +1,40 @@
 import java.util.*;
 
 class Solution {
-    static Map<Integer,Integer> map = new HashMap<>();
+    
+    static Map<Integer,Integer> answer = new HashMap<>();
+    
     public int solution(String numbers) {
         
-        char[] num = new char[numbers.length()];
-        boolean[] isUsed = new boolean[numbers.length()];
-        
-        for(int i=0;i<numbers.length();i++){
-            num[i] = numbers.charAt(i);
+        int len = numbers.length();
+        int[] number = new int[len];
+    
+
+        for(int i=0;i<len;i++){
+            number[i] = numbers.charAt(i) - '0';
         }
         
-        dfs(num,"",isUsed);
-            
-        return map.size();
+        dfs(number,new boolean[len],len,"");
+        return answer.size();
     }
     
-    
-    public void dfs(char[] numbers,String value,boolean[] isUsed){
-        
-        if(!value.equals("")){
-            if(isPrime(Integer.parseInt(value)))map.put(Integer.parseInt(value),1);
-            
-        }
-        
-        for(int i=0;i<numbers.length;i++){
+    public void dfs(int[] number,boolean[] isUsed,int len,String value){
+        for(int i=0;i<len;i++){
             if(!isUsed[i]){
                 isUsed[i] = true;
-                dfs(numbers,value+numbers[i],isUsed);
+                String tempValue = value + number[i];
+                if(isPrime(Integer.parseInt(tempValue))) answer.put(Integer.parseInt(tempValue),1);
+                dfs(number,isUsed,len,tempValue);
                 isUsed[i] = false;
             }
         }
     }
     
-    
-    public boolean isPrime(int n){
+    private boolean isPrime(int n){
+        int temp = (int)Math.sqrt(n);
         
-        if(n==0 || n==1) return false;
-        int num = (int)Math.sqrt((double)n);
-        
-        for(int i=2;i<=num;i++){
+        if(n == 0 || n == 1) return false;
+        for(int i=2;i<=temp;i++){
             if(n%i == 0) return false;
         }
         
