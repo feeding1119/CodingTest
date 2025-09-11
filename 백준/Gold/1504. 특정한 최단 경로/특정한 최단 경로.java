6eb63a789f272e76c516226412b2fe57ee013bf9
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2025-09-11 18:27:26 +0900

    [Gold IV] Title: 특정한 최단 경로, Time: 536 ms, Memory: 68456 KB -BaekjoonHub

--- 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
+++ 백준/Gold/1504. 특정한 최단 경로/특정한 최단 경로.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,29 +7,29 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static class Node{
-        int idx;
-        int cost;
-
-        Node(int idx,int cost){
-            this.idx = idx;
-            this.cost = cost;
-        }
-    }
-
     static int N,E;
     static ArrayList<ArrayList<Node>> graph;
 
-    public static void main(String[] args) throws IOException {
+    static class Node implements Comparable<Node>{
+        int idx,cost;
 
+        public Node(int idx,int cost){
+            this.idx = idx;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(Node o){
+            return this.cost - o.cost;
+        }
+    }
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         N = Integer.parseInt(st.nextToken());
-
         E = Integer.parseInt(st.nextToken());
-
 
         graph = new ArrayList<>();
 
@@ -39,7 +38,6 @@ public class Main {
         }
 
         for(int i=0;i<E;i++){
-
             st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
@@ -82,44 +80,43 @@ public class Main {
         if(answer > ans2) answer = ans2;
 
         System.out.println(answer);
-
-
     }
 
     public static int dijkstra(int start,int end){
-        boolean[] visited = new boolean[N+1];
-        int[] dist = new int[N+1];
+        boolean[] v = new boolean[N+1];
+        int[] cost = new int[N+1];
 
         for(int i=0;i<=N;i++){
-            dist[i] = Integer.MAX_VALUE;
+            cost[i] = Integer.MAX_VALUE;
         }
 
-        dist[start] = 0;
+        cost[start] = 0;
 
         for(int i=0;i<N;i++){
 
             int nodeValue = Integer.MAX_VALUE;
             int nodeIdx = 0;
 
-            for(int j=1;j<N+1;j++){
-                if(!visited[j] && dist[j] < nodeValue){
-                    nodeValue = dist[j];
+            for(int j=1;j<=N;j++){
+                if(!v[j] && cost[j] < nodeValue){
+                    nodeValue = cost[j];
                     nodeIdx = j;
                 }
             }
 
-            visited[nodeIdx] = true;
+            v[nodeIdx] = true;
 
             for(int j=0;j<graph.get(nodeIdx).size();j++){
 
                 Node adjNode = graph.get(nodeIdx).get(j);
-                if(dist[adjNode.idx] > dist[nodeIdx] + adjNode.cost){
-                    dist[adjNode.idx] = dist[nodeIdx] + adjNode.cost;
+
+                if(cost[adjNode.idx] > nodeValue + adjNode.cost){
+                    cost[adjNode.idx] = nodeValue + adjNode.cost;
                 }
+
             }
         }
 
-        return dist[end];
-
+        return cost[end];
     }
 }
