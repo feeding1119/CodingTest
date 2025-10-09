973547caf4baf95720e1d22fee5da4c0ba53347e
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2025-10-10 08:01:54 +0900

    [Gold III] Title: 줄 세우기, Time: 736 ms, Memory: 55168 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -1,5 +1,4 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -14,17 +13,18 @@ public class Main {
 
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
 
-        int[] indegree = new int[n+1];
+        int[] indegree = new int[N+1];
+
         ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
 
-        for(int i=0;i<=n;i++){
+        for(int i=0;i<N+1;i++){
             graph.add(new ArrayList<>());
         }
 
-        for(int i=0;i<m;i++){
+        for(int i=0;i<M;i++){
             st = new StringTokenizer(br.readLine());
 
             int a = Integer.parseInt(st.nextToken());
@@ -37,22 +37,25 @@ public class Main {
         Queue<Integer> q = new LinkedList<>();
         ArrayList<Integer> answer = new ArrayList<>();
 
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<N+1;i++){
             if(indegree[i] == 0) q.offer(i);
         }
 
         while(!q.isEmpty()){
             int now = q.poll();
-            answer.add(now);
 
-            for(int next : graph.get(now)){
-                indegree[next]--;
-                if(indegree[next] == 0) q.offer(next);
+            for(int g:graph.get(now)){
+                indegree[g]--;
+
+                if(indegree[g] == 0) q.offer(g);
             }
+
+            answer.add(now);
         }
 
-        for(int h : answer){
-            System.out.print(h+" ");
+        for(int i=0;i<answer.size();i++){
+            System.out.print(answer.get(i)+" ");
         }
     }
+
 }
