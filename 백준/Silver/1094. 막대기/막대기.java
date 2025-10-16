717c32ff6f87f14480cc17af92b4a6810c9be5cc
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2025-10-16 10:27:32 +0900

    [Silver V] Title: 막대기, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -7,16 +7,7 @@ import java.io.InputStreamReader;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int X = Integer.parseInt(br.readLine());
-
-        String xb = Integer.toBinaryString(X);
-
-        int answer = 0;
-        for(int i=0;i<xb.length();i++){
-            if(xb.charAt(i) == '1') answer++;
-        }
-
-        System.out.println(answer);
+        
+        System.out.println(Integer.bitCount(Integer.parseInt(br.readLine())));
     }
 }
