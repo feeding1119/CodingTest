81e3d98a11e32173d7e2c001a9590dc89b93288a
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2026-03-30 18:50:46 +0900

    [Gold V] Title: 평범한 배낭, Time: 184 ms, Memory: 53856 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -1,40 +1,39 @@
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
-        int[][] dp = new int[n+1][k+1];
-        int[][] item = new int[n][2];
+        int[] w = new int[n+1];
+        int[] v = new int[n+1];
 
-        for(int i=0;i<n;i++){
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
-
-            int w = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            item[i][0] = w;
-            item[i][1] = v;
+            w[i] = Integer.parseInt(st.nextToken());
+            v[i] = Integer.parseInt(st.nextToken());
         }
 
+        int[][] dp = new int[n+1][k+1];
 
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=k;j++){
-                dp[i][j] = dp[i-1][j];
-
-                if(j>=item[i-1][0]) dp[i][j] = Math.max(dp[i][j],dp[i-1][j-item[i-1][0]]+item[i-1][1]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= k; j++) {
+                // 1. 안 넣는 경우
+                dp[i][j] = dp[i - 1][j];
+                // 2. 넣는 경우
+                if (j >= w[i]) {
+                    dp[i][j] = Math.max(
+                            dp[i][j],
+                            dp[i - 1][j - w[i]] + v[i]
+                    );
+                }
             }
         }
 
