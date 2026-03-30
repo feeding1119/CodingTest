c21b9f7b727ffd410c61b884c064327a985e0b88
feeding1119 <58655693+feeding1119@users.noreply.github.com>
2026-03-30 18:14:50 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 576 ms, Memory: 49036 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.java
@@ -1,3 +1,4 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -8,10 +9,9 @@ public class Main {
 
     static int V,E;
     static int[] parent;
-    static Edge edges[];
-    static int answer = 0;
+    static Edge[] edges;
 
-    public static class Edge implements Comparable<Edge>{
+    static class Edge implements Comparable<Edge>{
         int u,v,c;
 
         public Edge(int u,int v,int c){
@@ -25,6 +25,7 @@ public class Main {
             return this.c - o.c;
         }
     }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -34,22 +35,25 @@ public class Main {
         E = Integer.parseInt(st.nextToken());
 
         parent = new int[V+1];
+        edges = new Edge[E];
 
-        for(int i=1;i<=V;i++){
+        for(int i=1;i<V+1;i++){
             parent[i] = i;
         }
 
-        edges = new Edge[E];
-
         for(int i=0;i<E;i++){
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
             int c = Integer.parseInt(st.nextToken());
-            edges[i] = new Edge(u,v,c);
+
+            edges[i] = new Edge(a,b,c);
         }
 
         Arrays.sort(edges);
+
+        int answer = 0;
 
         for(int i=0;i<E;i++){
             Edge now = edges[i];
@@ -59,8 +63,11 @@ public class Main {
         }
 
         System.out.println(answer);
-    }
 
+
+
+
+    }
     static int find(int x){
         if(parent[x] == x) return x;
         return parent[x] = find(parent[x]);
@@ -73,4 +80,5 @@ public class Main {
         if(x>y) parent[x] = y;
         else parent[y] = x;
     }
+
 }
